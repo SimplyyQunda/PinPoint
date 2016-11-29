f555fa5acab47a8e27d0bcde655271b518c9a543
DaQunda Magwood <dmagwood13@gmail.com>
2016-11-29 15:08:20 -0500

    modules are setup

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 import angular from 'angular';
+import client_id from "./token";
 
-import { CategoryController } from './controllers/catergory';
+import { routerConfig } from './route'
+
+import { CategoryController } from './controller/category';
 
 import 'angular-ui-router';
 
 angular
   .module('app', ['ui.router'])
+  .config(routerConfig)
   .controller('CategoryController', CategoryController)
