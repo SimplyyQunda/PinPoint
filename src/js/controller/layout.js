5af7ef0e4643b0829ae21f2e762a61eb202b0d58
DaQunda Magwood <dmagwood13@gmail.com>
2016-11-29 20:26:27 -0500

    routers are complete

--- src/js/controller/layout.js
+++ src/js/controller/layout.js
@@ -5,7 +5,7 @@ function LayoutController () {
   vm.clickMe = clickMe;
 
   function clickMe () {
-    // console.log("its been clicked");
+    console.log("its been clicked");
   }
 }
 
--- src/js/route.js
+++ src/js/route.js
@@ -2,12 +2,27 @@ import client_id from "./token";
 
 function routerConfig ($stateProvider, $urlRouterProvider){
 	$stateProvider
-		.state('home', {
-	      url: '/',
+	    .state('root', {
+	      abstract: true,
+	      templateUrl: 'templates/layout.tpl.html',
+	      controller: 'LayoutController as layout'
+	    })
+		.state('root.home', {
+	      url: '/home',
 	      templateUrl: 'templates/home.tpl.html',
 	      controller: 'HomeController as home'
 	    })
-	    .state('category', {
+	    .state('root.profile', {
+	      url: '/profile',
+	      templateUrl: 'templates/profile.tpl.html',
+	      controller: 'ProfileController as profiles'
+	    })
+	    .state('root.about', {
+	      url: '/about',
+	      templateUrl: 'templates/about.tpl.html'
+	    })
+
+	    .state('root.category', {
 	      url: '/category/:id',
 	      templateUrl: 'templates/category.tpl.html',
 	      controller: 'CategoryController as categories'
