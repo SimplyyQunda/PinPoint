329123b0672d091d2620951b7e0e421262b86626
DaQunda Magwood <dmagwood13@gmail.com>
2016-12-01 10:48:23 -0500

    login files has been completed

---
+++ src/js/controller/login.js
@@ -0,0 +1,23 @@
+function LoginController (CategoryService, $state, $rootScope) {
+
+  let vm = this;
+
+  vm.activate = activate;
+
+  function activate (user) {
+    CategoryService.login(user).then(
+      resp => {
+        CategoryService.setUser(resp.data);
+        $rootScope.$broadcast('loginChange', {});
+        $state.go('root.home');
+      },
+      errors => {
+        console.log(errors.data.error);
+      }
+    );
+  };
+
+};
+
+LoginController.$inject = ['CategoryService', '$state', '$rootScope'];
+export { LoginController };
